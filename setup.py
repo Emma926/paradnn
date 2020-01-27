2a12f1f03a2d9adac1abe26389b140d4285a68e3
Emma <emmawong926@gmail.com>
2020-01-27 11:03:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-pip install tensorflow==1.13
+from setuptools import setup
+
+setup(
+   name='paradnn',
+   version='1.0',
+   description='A tool that generates parameterized deep neural network models. It provides large “end-to-end” models covering current and future applications, and parameterizing the models to explore a much larger design space of DNN model attributes.',
+   author='Emma Wang',
+   author_email='emmawong926@gmail.com',
+   packages=['paradnn'],
+   install_requires=['python3', 'tensorflow'],
+)
